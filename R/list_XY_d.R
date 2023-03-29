cadc8cfcb14ab20075e87affcb3535ed24eeb813
Ekaterina <ekaterina.tomilina@inrae.fr>
2023-03-29 11:49:41 +0200

    new function for list XYd

--- R/list_XY_d.R
+++ R/list_XY_d.R
@@ -31,45 +31,38 @@ matrice_diag_blocs <- function(blocs, coeff){
 }
 
 
-#' list_XY_d
+#' CopulaSim
 #'
 #' @description multidimensional data simulator based on the generalized inverse method
 #'
 #' @param n Integer, length of the simulated data
 #' @param blocks vector, vector with the size of the blocks on the diagonal of the matrix
 #' @param coeff vector, coefficient associated to each block (there is no need to inform the coefficient for one dimensional block (see examples))
-#' @param list_qlaws  List, list of  quasi-inverse functions (must be chosen among (qbeta, qbinom, qchisq, qubif, qexp, qf, qgamma, qgeom, qhyper, qnbinom, qnorm, qpois, qt, qweibull))
-#' @param list_param List, list with lists of parameters
+#' @param list_qlaws  List, list of  quasi-inverse functions (must be chosen among (qbeta, qbinom, qchisq, qubif, qexp, qf, qgamma, qgeom, qhyper, qnbinom, qnorm, qpois, qt, qweibull)) with their specified parameters
 #' @param repetition vector, number of repetition of each laws
 #' @param random if TRUE the distribution of each laws will be randomly distibuted among the blocks. if FALSE the order enter as a parameter will be preserved. (by default : random = TRUE)
 #'
-#' @return list of d vectors of length n such that the joint distribution function of those vectors is given  by F(.,.) = C(F_1(.),..., F_d(.)) where C is the gaussian Copula related to a block diagonal matrix and F_1,.., F_d are respectively the distribution functions of the margins.
-#' @examples list_XY_d(100, c(3,1,4,5), c(0.5,0.8,0.3), list(qexp, qnorm, qbinom), list(3, c(0,1), c(20,0.5)), c(5,4,4), random = TRUE)
+#' @return matrix of d vectors (in columns) of length n such that the joint distribution function of those vectors is given  by F(.,.) = C(F_1(.),..., F_d(.)) where C is the gaussian Copula related to a block diagonal matrix and F_1,.., F_d are respectively the distribution functions of the margins.
+#' @examples list_XY_d(100, c(3,1,4,5), c(0.2,0.1,0.5,0.3), list(function(p){qexp(p=p, rate=1)}, function(p){qnorm(p=p, mean=0, sd=1)}, function(p){qnorm(p=p, mean=0.76, sd=2)}), c(5,4,4), random = TRUE)
 #' @export
 #'
-list_XY_d <- function(n, blocks, coeff, list_qlaws, list_param, repetition, random = TRUE){
+CopulaSim <- function(n, blocks, coeff, list_qlaws, repetition, random = TRUE){
 
   R = matrice_diag_blocs(blocks, coeff)
   d = sum(blocks)
-  XY = gaussCopule_(VecEstim(R),d,n)
-
+  XY = gaussCopule(R,n)
+  
   nb = c()
   for (l in 1:length(repetition))
     nb = append(nb, rep(l,repetition[l]))
   if (random == TRUE){
     nb = sample(nb)
   }
-
-  liste_Xs = c()
+  
+  liste_Xs = matrix(0,n,d)
   for (j in 1:d){
     i = nb[j]
-    if (length(list_param[[i]]) == 2){
-      liste_Xs[[j]] = list_qlaws[[i]](XY[,j], list_param[[i]][1], list_param[[i]][2])
-    }
-    else if (length(list_param[[i]]) == 3){
-      liste_Xs[[j]] = list_qlaws[[i]](XY[,j], list_param[[i]][1], list_param[[i]][2], list_param[[i]][3])
-    }
-    else {liste_Xs[[j]] = list_qlaws[[i]](XY[,j], list_param[[i]])}
+    liste_Xs[,j] = list_qlaws[[i]](XY[, i])
   }
   return(liste_Xs)
 }
