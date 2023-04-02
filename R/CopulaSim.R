b2afdc19c6876b8dd077b78087e16f02a173167f
Ekaterina <ekaterina.tomilina@inrae.fr>
2023-04-02 17:35:30 +0200

    nouveau nom CopulaSim

--- R/CopulaSim.R
+++ R/CopulaSim.R
@@ -7,11 +7,18 @@ VecEstim <- function(Corr){
   return(rho)
 }
 
-gaussCopule_ <- function(vec,d,n){
-  norm.cop <- copula::normalCopula(vec, dim = d, dispstr = "un")
-  XY_g = data.frame(copula::rCopula(n, norm.cop))
-  return(as.matrix(XY_g))
+gaussCopule<- function(R, n){
+  d = dim(R)[1]
+  A = as.matrix(eigen(R)$vectors%*%diag(sqrt(eigen(R)$values)))
+  data = matrix(0, n, d)
+  for (i in c(1:n)){
+    z = as.matrix(rnorm(d, mean = 0, sd = 1))
+    x = pnorm(A%*%z)
+    data[i,] = t(x)}
+  data = data.frame(data)
+  return(data)
 }
+
 
 matrice_diag_blocs <- function(blocs, coeff){
   d = sum(blocs)
@@ -27,7 +34,7 @@ matrice_diag_blocs <- function(blocs, coeff){
     }
   }
   for (i in 1:d){R[i,i] = 1}
-  return(R)
+  return(as.matrix(R))
 }
 
 
@@ -43,7 +50,7 @@ matrice_diag_blocs <- function(blocs, coeff){
 #' @param random if TRUE the distribution of each laws will be randomly distributed among the blocks. if FALSE the order enter as a parameter will be preserved. (by default : random = TRUE)
 #'
 #' @return matrix of d vectors (in columns) of length n such that the joint distribution function of those vectors is given  by F(.,.) = C(F_1(.),..., F_d(.)) where C is the gaussian Copula related to a block diagonal matrix and F_1,.., F_d are respectively the distribution functions of the margins.
-#' @examples list_XY_d(100, c(3,1,4), c(0.2,0.1,0.5), list(function(p){qexp(p=p, rate=1)}, function(p){qnorm(p=p, mean=0, sd=1)}, function(p){qnorm(p=p, mean=0.76, sd=2)}), c(5,4,4), random = TRUE)
+#' @examples Copulasim(100, c(3,1,4), c(0.2,0.1,0.5), list(function(p){qexp(p=p, rate=1)}, function(p){qnorm(p=p, mean=0, sd=1)}, function(p){qnorm(p=p, mean=0.76, sd=2)}), c(5,4,4), random = TRUE)
 #' @export
 #'
 CopulaSim <- function(n, blocks, coeff, list_qlaws, repetition, random = TRUE){
@@ -69,5 +76,5 @@ CopulaSim <- function(n, blocks, coeff, list_qlaws, repetition, random = TRUE){
 
 #'
 
-
+CopulaSim(100, c(3,1,4), c(0.2,0.1,0.5), list(function(p){qexp(p=p, rate=1)}, function(p){qnorm(p=p, mean=0, sd=1)}, function(p){qnorm(p=p, mean=0.76, sd=2)}), c(5,4,4), random = TRUE)
 
